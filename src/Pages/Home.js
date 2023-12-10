9acc696d9fd21d2d6b7cbc81ad5825fc00771c12
Khaqan Ashraf <khaqan_ashraf@yahoo.com>
2023-12-10 22:13:07 +0500

    imports removed from home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,9 @@
-import Slider from '../components/Slider'
-import Products from '../components/Products'
-import Newsletter from '../components/Newsletter'
+
 import Footer from '../components/Footer'
-import Categories from '../components/Categories'
 import Header from '../components/Header'
-import ProductSlider from '../components/ProductSlider'
-import { getBusinessId, getBusinessName, getDiscountedProducts, getLatestProducts, getPopularCategories, getPopularProducts } from '../api/utils'
 import { BusinessData } from '../theme/BusinessWrapper'
 import styled from 'styled-components'
-import { useEffect } from 'react'
-import { categorySliderBuilder } from '../theme/HomepageBuilder/CategorySliderBuilder'
-import { productSliderBuilder } from '../theme/HomepageBuilder/ProductSliderBuilder'
-import { SectionFactory, SliderFactory } from '../theme/HomepageBuilder/SectionFactory'
-import { customSectionBuilder } from '../theme/HomepageBuilder/CustomSectionBuilder'
-import { mobile } from '../responsive'
-import Malenian from '../components/CustomSections/Malenian'
-import { CustomSectionFactory } from '../components/CustomSections/Factory'
+import { SectionFactory } from '../theme/HomepageBuilder/SectionFactory'
 
 const HomePageWrapper = styled.div`
 
